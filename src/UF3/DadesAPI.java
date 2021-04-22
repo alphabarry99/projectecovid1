2945bcdb7fef5893e8888ba3124a0bc14d8549f6
Albert <albertbuch979@gmail.com>
2021-04-22 08:49:59 +0200

    Update DadesApi
    Canvi en com demanem el día que volem veure les dades

--- src/UF3/DadesAPI.java
+++ src/UF3/DadesAPI.java
@@ -9,11 +9,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class DadesAPI {
 
     public void dadesCatalunya() throws IOException, ParseException {
-        String URL1 = "https://api.covid19tracking.narrativa.com/api/2020-04-19/country/spain/region/cataluna";
+        System.out.println("Quin día vols veure (Format: AAAA-MM-DD)");
+        Scanner in = new Scanner(System.in);
+        String dataPrint = in.nextLine();
+
+        String URL1 = "https://api.covid19tracking.narrativa.com/api/"+dataPrint+"/country/spain/region/cataluna";
 
         URL url = new URL(URL1);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -21,12 +26,12 @@ public class DadesAPI {
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = (JSONObject) parser.parse(new InputStreamReader(connection.getInputStream()));
         JSONObject data = (JSONObject) jsonObject.get("dates");
-        JSONObject fecha = (JSONObject) data.get("2020-04-19");
+        JSONObject fecha = (JSONObject) data.get(dataPrint);
         JSONObject countries = (JSONObject) fecha.get("countries");
         JSONObject Spain = (JSONObject) countries.get("Spain");
         JSONArray regions = (JSONArray) Spain.get("regions");
         JSONObject region = (JSONObject) regions.get(0);
-        System.out.println("A Catalunya hi han hagut : " + region.get("today_new_confirmed") + " casos de CoronaVirus avui, ahir " + region.get("date"));
+        System.out.println("A Catalunya hi han hagut : " + region.get("today_new_confirmed") + " casos de CoronaVirus ahir : " + region.get("date"));
     }
 }
 
