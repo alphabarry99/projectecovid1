e22240a38aa404773fff46aeaa6998dc5f63550c
alphabarry99 <a.barry@sapalomera.cat>
2021-04-27 10:20:30 +0200

    lectura de taulell 1

--- src/UF3/LecturaFitxer.java
+++ src/UF3/LecturaFitxer.java
@@ -13,33 +13,17 @@ public class LecturaFitxer {
     src.UF3.Taulell t = new Taulell();
 
     public String getLecturaFitxer() {
-        float[][] llegirtaulell = new float[0][0];
-        try {
-            File f = new File("res/taulells.txt");
-            Scanner in = new Scanner(f);
-            String espai = in.nextLine();
-            String[] separador = espai.split(" ");
-            files = Integer.parseInt(separador[0]);
-            taulell.setFiles(files);
-            columnes = Integer.parseInt(separador[1]);
-            taulell.setColumnes(columnes);
-            llegirtaulell = new float[files][columnes];
-            espai = in.nextLine();
-            separador = espai.split(" ");
-            for (int i = 0; i < files; i++) {
-                for (int j = 0; j < columnes; j++) {
-                    llegirtaulell[i][j] = Float.parseFloat(String.valueOf(separador[j]));
-                    System.out.print(llegirtaulell[i][j]);
+            try {
+                File f = new File("res/taulells.txt");
+                Scanner in = new Scanner(f);
+
+                while (in.hasNextLine()){
+                    System.out.println(in.nextLine());
                 }
-                espai = in.nextLine();
-                separador = espai.split(" ");
+            }catch (Exception e){
+                System.out.println(e.getMessage());
             }
 
-            taulell.setTaulell(llegirtaulell);
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return toString();
+        return null;
     }
 }
